bc057f42269ac98c83db4bdc0ed27b4ea22e8fcd
Chomu999 <84760026+Chomu999@users.noreply.github.com>
2022-07-15 11:35:34 +0530

    Update controll.js

--- controll.js
+++ controll.js
@@ -1,9 +1,11 @@
+//resize fun
+
+
+// touch input
+
 
 cvs.addEventListener(touch[0],(e)=>{
-
 MOUSE.x = e.touches[0].clientX - crr.canvas.x
-
-
 movin.goin = true;
 
 })
@@ -11,20 +13,101 @@ movin.goin = true;
 
 
 cvs.addEventListener(touch[1],(e)=>{
-
-
 MOUSE.x = e.touches[0].clientX - crr.canvas.x
-
 movin.goin = true;
-
 })
 
 
 
 cvs.addEventListener(touch[2],(e)=>{
 movin.goin = false;
+})
+
+
+//mouse input
+
+
+cvs.addEventListener(mouseT[0],(e)=>{
+MOUSE.x = e.offsetX
+MOUSE.y = e.offsetY
+movin.goin = true;
+})
+
+
+cvs.addEventListener(mouseT[1],(e)=>{
+MOUSE.x = e.offsetX
+MOUSE.y = e.offsetY
+movin.goin = true;
+})
+
+cvs.addEventListener(mouseT[2],(e)=>{
+movin.goin = false;
+})
+
+
+
+
+
+
+//keybroad input
+
+
+window.addEventListener('keydown',(e)=>{
+
+switch(e.key){
+
+case 'a':
+movin.left=true;
+break;
+
+case 'd':
+movin.right=true;
+break;
+
+case 'ArrowLeft':
+movin.left=true;
+break;
+
+case 'ArrowRight':
+movin.right=true;
+break;
+
+}
 
 })
+
+
+
+window.addEventListener('keyup',(e)=>{
+
+switch(e.key){
+case 'a':
+movin.left=false;
+break;
+
+case 'd':
+movin.right=false;
+break;
+
+case 'ArrowLeft':
+movin.left=false;
+break;
+
+case 'ArrowRight':
+movin.right=false;
+break;
+
+}
+
+
+})
+
+
+
+
+
+
+
 
 
 pre.addEventListener('dblclick',()=>{
@@ -33,4 +116,6 @@ miniBox.requestFullscreen()
 
 
 
-addEventListener('resize',reSize);
+window.addEventListener('resize',reSize);
+
+
